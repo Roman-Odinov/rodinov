8df9d578c47e61816f5b6c587f4de5638757b73b
Roman Odinov <roman.odinov@yandex.ru>
2017-07-26 18:46:46 +0300

    JavaDoc + Code convention

--- code/chapter_001/src/main/java/ru/job4j/Calculate.java
+++ code/chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,12 +1,30 @@
 package ru.job4j;
 
+/**
+* class Calculate : задачи к Части 001
+*/
 public class Calculate {
+	/**
+	* echo : метод для тестирования
+	* @param value : строка для вывода в консоль
+	* @return String value
+	*/
 	public String echo(String value){
-		return String.format("%s", value);
+		if (value != "") {
+			value = String.format("%s", value);
+		} else {
+			value = "the string is empty!";
+		}
+		return value;
 	}
-		
+	
+	/**
+	* Конструктор : передача строки и получение результатов 
+	* @param args - args
+	*/
 	public static void main (String[] args){
-		Calculate calc = new Calculate();					// new instance of the Calculate class
-		System.out.println(calc.echo("Hello, World!"));		//call 'echo' method
+		Calculate calc = new Calculate();
+		System.out.println(calc.echo(""));	// empty string test
+		System.out.println(calc.echo("Hello, World!"));
 	}
 }
