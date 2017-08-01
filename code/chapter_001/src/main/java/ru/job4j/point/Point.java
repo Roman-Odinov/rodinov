26f1bed15aeaa4c9d750f89edec7ec1993a6a7f8
Roman Odinov <roman.odinov@yandex.ru>
2017-08-01 11:03:15 +0300

    Положение точки

---
+++ code/chapter_001/src/main/java/ru/job4j/point/Point.java
@@ -0,0 +1,33 @@
+package ru.job4j.point;
+
+/**
+* Задано уравнение функции y(x) = a * x + b.
+* например: a=2 b=1 x=5
+* y = 11
+*/
+
+
+public class Point {
+  private int x;
+  private int y;
+
+  public Point(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public int getX() {
+    return this.x;
+  }
+
+  public int getY() {
+    return this.y;
+  }
+
+  public boolean equationCorrect(int a, int b) {
+	return (getY() == a * getX() + b);
+  }
+
+
+}
+
