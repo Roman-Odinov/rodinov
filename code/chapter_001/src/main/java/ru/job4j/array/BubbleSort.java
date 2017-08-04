7db691cd5154225ef694a2cfb1feb7d60be8955e
Roman Odinov <roman.odinov@yandex.ru>
2017-08-04 11:01:31 +0300

    Создать программу для сортировки массива методом перестановки.

---
+++ code/chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -0,0 +1,64 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+/**
+ * алгоритм сортировки пузырьком.
+ * Метод swap - замена 2-х элементов в массиве;
+ * Метод sort - сортировка пузырьковым методом.
+ */
+public class BubbleSort {
+
+    /**
+     * Метод swap - замена 2-х элементов в массиве.
+     * @param i - индекс одного элемента
+     * @param j - индекс второго элемента
+     * @param arr - массив.
+     */
+    private void swap(int i, int j, int[] arr) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    /**
+     * Сортировка массива целых чисел, используя алгоритм сортировки пузырьком от меньшего к бОльшему.
+     * @param intArray - входящий массив;
+     * @return - сортированный массив;
+     */
+    public int[] sort(int[] intArray) {
+        int arrSize = intArray.length;
+        // делаем копию масива
+        int[] array = new int[arrSize];
+        System.arraycopy( intArray, 0, array, 0, arrSize );
+
+        for(int j = 0; j < arrSize - 1; j++) {
+            boolean flipped = false;
+            for(int i = 0; i < arrSize - 1 - j; i++) {
+                if (array[i] > array[i+1]) {
+                    swap(i, i+1, array);
+                    flipped = true;
+                }
+            }
+            if (!flipped) {
+                break;
+            }
+        }
+        return array;
+    }
+
+    /**
+     * Для отладки!
+     * @param args
+     */
+    /*
+    public static void main(String[] args) {
+        int[] intArr = { 1, 118, 5, 4, 16, 7, 9 };
+        int[] sorted = new BubbleSort().sort(intArr);
+        System.out.println(intArr);
+        System.out.println(sorted);
+        System.out.println(Arrays.toString(intArr));
+        System.out.println(Arrays.toString(sorted));
+    }
+    */
+}
---
+++ code/chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Тесты для метода back класса BubbleSort.
+ */
+public class BubbleSortTest {
+    @Test
+    public void sortTest() throws Exception {
+        int[] intArray = { 1, 118, 5, 4, 16, 7, 9 };
+        int[] resultArray = new BubbleSort().sort(intArray);
+        int[] expectedArray = { 1, 4, 5, 7, 9, 16, 118 };
+        assertThat(resultArray, is(expectedArray));
+    }
+
+}
