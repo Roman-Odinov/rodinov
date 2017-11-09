1ff2b0b87f3ff04daea7601f41b59460c02d065e
Roman-Odinov <roman.odinov@yandex.ru>
2017-11-09 16:11:43 +0300

    Перекрывать hashCode

--- code/chapter_005/src/main/java/Map/User.java
+++ code/chapter_005/src/main/java/Map/User.java
@@ -3,11 +3,13 @@ package Map;
 import java.util.*;
 
 /**
+ * Перекрывать hashCode.
+ *
  * Создать два объекта User, которые имеют одинаковые поля.
  *
  * Создать карту Map<User, Object>
  *
- * Добавить два объекта. Вывести карту на печать. Описать полученный результат словами.
+ * Добавить два объекта с пункта 3 в карту. Вывести карту на печать. Описать полученный результат словами.
  */
 public class User {
 
@@ -21,12 +23,32 @@ public class User {
         this.birthday = birthday;
     }
 
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//
+//        User user = (User) o;
+//
+//        if (children != user.children) return false;
+//        if (!name.equals(user.name)) return false;
+//        return birthday.equals(user.birthday);
+//    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + children;
+        result = 31 * result + birthday.hashCode();
+        return result;
+    }
 
     public static void main(String[] args) {
         User user1 = new User("roman", 0, new GregorianCalendar(1980, 12, 21));
         User user2 = new User("roman", 0, new GregorianCalendar(1980, 12, 21));
         System.out.println("equals: " + user1.equals(user2));
-//        System.out.println(hashCode(user1));
+        System.out.println("user1.hashCode() = " + user1.hashCode());
+        System.out.println("user2.hashCode() = " + user2.hashCode());
 
         Map<User, String> map = new HashMap<>();
         map.put(user1, "first");
